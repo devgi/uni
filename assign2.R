742f18258d8185187a01b697c28e7029c8ae279f
Dan Evgi <dane@guardicore.com>
2021-05-31 00:20:43 +0300

    add some transofrmations + compute some regressions

--- assign2.R
+++ assign2.R
@@ -13,7 +13,7 @@ dim(cerealDf)
 summary(cerealDf)
 View(cerealDf)
 
-# In the following visualizations limegreen would represent success - meaning 
+# In the following visualizations limegreen would represent success - meaning
 # customer preferred product A 
 
 plot(cerealDf$LoyaltyB ~ cerealDf$LoyaltyA, data=cerealDf,
@@ -45,3 +45,55 @@ plot(cerealDf$PriceB ~ cerealDf$DiscountB, data=cerealDf,
 ########################################
 # 2. Prepare Data
 ########################################
+
+cerealDf$normLoyality <- as.numeric(cerealDf$LoyaltyA / (cerealDf$LoyaltyA + cerealDf$LoyaltyB))
+cerealDf$isPreferredA <- as.numeric(ifelse(cerealDf$Brand.Preference == "A", 1, 0))
+
+plot(cerealDf$normLoyality, 
+     col=ifelse(cerealDf$isPreferredA, "limegreen", "red"))
+# NOTE: good 
+
+
+cerealDf$fullPriceA <- cerealDf$PriceA + cerealDf$DiscountA
+cerealDf$fullPriceB <- cerealDf$PriceB + cerealDf$DiscountB
+cerealDf$fullPriceAratio <- cerealDf$fullPriceA / cerealDf$fullPriceB
+plot(cerealDf$fullPriceA ~ cerealDf$fullPriceB, 
+     col=ifelse(cerealDf$isPreferredA, "limegreen", "red"))
+plot(cerealDf$fullPriceAratio, 
+     col=ifelse(cerealDf$isPreferredA, "limegreen", "red"))
+# NOTE: bad
+
+
+cerealDf$discountRatioA <- cerealDf$DiscountA / cerealDf$PriceA
+cerealDf$discountRatioB <- cerealDf$DiscountB / cerealDf$PriceB
+plot(cerealDf$discountRatioA ~ cerealDf$discountRatioB, 
+     col=ifelse(cerealDf$isPreferredA, "limegreen", "red"))
+# NOTE: pretty good
+
+View(cerealDf)
+
+
+########################################
+# 3. Model
+########################################
+
+reg1 <- lm(cerealDf$isPreferredA ~ cerealDf$DisplayA + cerealDf$DisplayB + cerealDf$normLoyality, data=cerealDf)
+summary(reg1)
+
+
+reg2 <- lm(cerealDf$isPreferredA ~ cerealDf$DisplayA + cerealDf$DisplayB + cerealDf$normLoyality+ 
+            cerealDf$PriceA  + cerealDf$PriceB + cerealDf$DiscountA + cerealDf$DiscountB
+          , data=cerealDf)
+summary(reg2)
+
+
+reg3 <- lm(cerealDf$isPreferredA ~ cerealDf$PriceA  + cerealDf$PriceB + cerealDf$DiscountA + cerealDf$DiscountB, 
+           data=cerealDf)
+summary(reg3)
+
+
+
+reg4 <- lm(cerealDf$isPreferredA ~ cerealDf$DisplayA + cerealDf$DisplayB + cerealDf$normLoyality+ 
+             cerealDf$discountRatioA + cerealDf$discountRatioB
+           , data=cerealDf)
+summary(reg4)
